7fb355291d0c16206e427e42567d60292cc2f64b
Matt Kirwan <lkjsad@fastmail.im>
2022-12-07 21:09:44 +0000

    Trying with the application plugin in the gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     java
+    application
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
+}
+
+
+application {
+    mainClass.set("com.sdca.api.SdcaApplication")
 }
 
 group = "com.sdca"
